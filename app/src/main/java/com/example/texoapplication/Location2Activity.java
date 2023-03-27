76e30d7766f640332cf2f5598fe250a48181b582
hasti2124 <120078504+hasti2124@users.noreply.github.com>
2023-03-27 08:57:58 +0530

    commit 1

---
+++ app/src/main/java/com/example/texoapplication/Location2Activity.java
@@ -0,0 +1,14 @@
+package com.example.texoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Location2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location2);
+    }
+}
